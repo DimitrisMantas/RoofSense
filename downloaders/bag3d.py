9dc45145045a22a7c8b3aac95b168785195b08eb
Dimitris Mantas <dimitris.mantas@outlook.com>
2024-01-25 16:04:08 +0100

    review the 3dbag downloader

--- downloaders/bag3d.py
+++ downloaders/bag3d.py
@@ -1,46 +1,83 @@
+from __future__ import annotations
+
+import re
+
 import requests
 
 import config
-import utils
+from utils.cjio import to_jsonl
+from utils.file import BlockingFileDownloader
+from utils.type import BoundingBoxLike
 
 
-# TODO - Reformat, finalize function and variable names, and add documentation.
-
-
-class DataType:
-    ITEM, TILE = range(2)
-
-
-def download(id_: str, type_: DataType = DataType.TILE) -> None:
-    if type_ == DataType.ITEM:
-        partial_path = f"{config.env('TEMP_DIR')}{id_}"
-        _download_item_data(id_, partial_path)
-    elif type_ == DataType.TILE:
-        partial_path = f"{config.env('TEMP_DIR')}{id_}"
-        _download_tile_data(id_, partial_path)
+def download(obj_id: str | BoundingBoxLike) -> None:
+    if _is_bbox_like(obj_id):
+        obj_type = _ObjectType.BBOX
     else:
-        raise ValueError(f"No such data type: {type_}")
+        obj_type = _get_object_type(obj_id)
+
+    partial_path = f"{config.env('TEMP_DIR')}"
+    if obj_type == _ObjectType.BBOX:
+        _download_bbox_data(obj_id, partial_path)
+    elif obj_type == _ObjectType.ITEM:
+        _download_item_data(obj_id, partial_path)
+    else:
+        _download_tile_data(obj_id, partial_path)
 
 
-def _download_item_data(id_: str, partial_path: str) -> None:
-    url = f"{config.var('BAG3D_API_BASE_URL')}{id_}"
-    filename = f"{partial_path}{config.var('CITY_JSON')}"
+def _is_bbox_like(obj_id: str):
+    return (
+        isinstance(obj_id, BoundingBoxLike.__origin__)
+        and len(obj_id) == 4
+        and all([(isinstance(i, int) or isinstance(i, float)) for i in BoundingBoxLike])
+    )
+
+
+_ITEM_ID = r"^NL\.IMBAG\.Pand\.\d{16}$"
+_TILE_ID = r"^\d{1,3}-\d{1,3}-\d{1,3}$"
+
+
+class _ObjectType:
+    BBOX, ITEM, TILE = range(3)
+
+
+def _get_object_type(obj_id: str) -> _ObjectType:
+    if re.match(_ITEM_ID, obj_id):
+        return _ObjectType.ITEM
+    elif re.match(_TILE_ID, obj_id):
+        return _ObjectType.TILE
+    raise ValueError(f"Invalid object ID : {obj_id}")
+
+
+def _download_bbox_data(obj_id: BoundingBoxLike, dirname: str) -> None:
+    raise NotImplementedError
+
+
+def _download_item_data(obj_id: str, dirname: str) -> None:
+    # TODO: Harmonize these variable names.
+    request = f"{config.var('BAG3D_API_BASE_URL')}{obj_id}"
+    pathnam = f"{dirname}{obj_id}{config.var('CITY_JSON')}"
 
     with requests.Session() as session:
-        utils.file.BlockingFileDownloader(
-            url, filename, session=session, callbacks=utils.cjio.to_jsonl
+        BlockingFileDownloader(
+            request, pathnam, session=session, callbacks=to_jsonl
         ).download()
 
 
-def _download_tile_data(id_: str, partial_path: str) -> None:
-    # Compile static, compound environment variables.
-    # FIXME - Do not recompile this constant at every function call.
-    _BASE_TILE_DATA_URL = (
+def _download_tile_data(obj_id: str, dirname: str) -> None:
+    # TODO: Promote this static constant to an environment variable.
+    _BASE_TILE_ADDRESS = (
         f"{config.var('BAG3D_TILE_URL')}{config.var('BAG3D_VER')}/tiles/"
     )
 
-    url = f"{_BASE_TILE_DATA_URL}{id_.replace('-', '/')}/{id_}{config.var('CITY_JSON')}"
-    filename = f"{partial_path}{config.var('CITY_JSON')}"
+    # TODO: Harmonize these variable names.
+    request = (
+        f"{_BASE_TILE_ADDRESS}"
+        f"{obj_id.replace('-', '/')}/"
+        f"{obj_id}"
+        f"{config.var('CITY_JSON')}"
+    )
+    pathnam = f"{dirname}{obj_id}{config.var('CITY_JSON')}"
 
     with requests.Session() as session:
-        utils.file.BlockingFileDownloader(url, filename, session=session).download()
+        BlockingFileDownloader(request, pathnam, session=session).download()
--- utils/type.py
+++ utils/type.py
@@ -1,7 +1,9 @@
 # TODO - Reformat, finalize function and variable names, and add documentation.
+from collections.abc import Sequence
 from typing import Any
 
 BAG3TileData = dict
 BAG3DTileIndexJSON = dict
 BAG3DTileIndexData = dict
 JSONLike = dict[str, Any]
+BoundingBoxLike = Sequence[float]
