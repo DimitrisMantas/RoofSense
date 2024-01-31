a03f71c52c4e7df72266e7b722faa58845e96856
Dimitris Mantas <dimitris.mantas@outlook.com>
2024-01-31 16:45:54 +0100

    rewrite lidar merger so that the output is consistent regardless of the input order

--- parsers/utils/pcloud.py
+++ parsers/utils/pcloud.py
@@ -7,6 +7,8 @@ from typing import Optional
 import laspy
 import laspy.lasappender
 import numpy as np
+import pandas as pd
+import rasterio
 import shapely
 import sklearn.neighbors
 
@@ -16,23 +18,24 @@ from utils.type import BoundingBoxLike
 
 class PointCloud:
     def __init__(self, path: str | PathLike) -> None:
-        self.index = None
         with laspy.open(path) as f:
             self.las = f.read()
 
-    def __len__(self) -> int:
-        return len(self.points)
+        self.index = None
 
     # TODO: Add type hints to this method.
     def __getitem__(self, key):
         if isinstance(key, int):
-            # Get the first point.
-            # NOTE: This indexing notation results in the point being returned in the
-            #       same format regardless of the key type.
+            # Fetch a single point record.
+            # NOTE: This indexing notation ensures that this method consistently
+            #       returns a PackedPointRecord instance.
             k = [key]
         else:
             k = key
         return self.points[k]
+
+    def __len__(self) -> int:
+        return len(self.points)
 
     @property
     def header(self) -> laspy.LasHeader:
@@ -52,42 +55,58 @@ class PointCloud:
         )
 
     def crop(self, bbox: BoundingBoxLike) -> PointCloud:
+        # NOTE: This operation must be performed using the original point records to
+        #       avoid floating-point comparisons.
+        #       The output is not affected because the transformation from records to
+        #       coordinates is linear.
         xmin = (bbox[0] - self.header.x_offset) / self.header.x_scale
         ymin = (bbox[1] - self.header.y_offset) / self.header.y_scale
         xmax = (bbox[2] - self.header.x_offset) / self.header.x_scale
         ymax = (bbox[3] - self.header.y_offset) / self.header.y_scale
         self.las.points = self.las.points[
             np.logical_and(
-                np.logical_and(xmin <= self.las.X, self.las.X <= xmax),
-                np.logical_and(ymin <= self.las.Y, self.las.Y <= ymax),
+                np.logical_and(xmin <= self["X"], self["X"] <= xmax),
+                np.logical_and(ymin <= self["Y"], self["Y"] <= ymax),
             )
         ]
         return self
 
-    # FIXME: Parallelize this function.
-    # TODO: Add type hints to this function.
-    # TODO: Optimize the search radius, power, and filler hyperparameters.
+    def remove_duplicates(self) -> PointCloud:
+        pts = np.vstack((self["X"], self["Y"], self["Z"])).transpose()
+        # NOTE: The points are sorted in descending elevation order so that the
+        #       highest ones are preserved.
+        pts = pts[np.argsort(-pts[:, 2])]
+        # NOTE: The unique element filter provided by NumPy is inefficient.
+        #       https://github.com/numpy/numpy/issues/11136
+        self.las.points = self.las.points[
+            pd.DataFrame(pts).drop_duplicates(subset=[0, 1]).index.tolist()
+        ]
+        return self
+
+    # TODO: Parallelize this method.
+    # TODO: Optimize the search radius, power, and fill search radius hyperparameters.
     def rasterize(
-        self, scalars: str | Sequence[str], size: float
-    ) -> raster.Raster | dict[str, raster.Raster]:
+        self,
+        scalars: str | Sequence[str],
+        resol: float,
+        meta: Optional[rasterio.profiles.Profile] = None,
+    ) -> dict[str, raster.Raster]:
         if isinstance(scalars, str):
             scalars = [scalars]
         # FIXME: Refactor ``xy()`` into a module function because doesn't make sense to
         #        create an empty raster.Raster just for the sake of using its accessors.
-        r = raster.Raster(size, self.bbox)
+        r = raster.Raster(resol, self.bbox)
 
-        # TODO: The point cloud should ensure that its spatial index has been initialized
-        #       before this call.
-        if self.index is None:
-            # noinspection PyUnresolvedReferences
-            self.index = sklearn.neighbors.KDTree(
-                np.vstack((self.las.x, self.las.y)).transpose()
-            )
+        self._init_index()
         neighbors, distances = self.index.query_radius(
-            r.xy(), r=size, return_distance=True
+            r.xy(), r=resol, return_distance=True
         )
 
-        rasters = {scalar: raster.Raster(size, self.bbox) for scalar in scalars}
+        rasters = {
+            scalar: raster.Raster(resol, bbox=self.bbox, meta=meta)
+            for scalar in scalars
+        }
+        # FIXME: Review the rest of this method.
         for cell_id, cell_nb in enumerate(neighbors):
             if len(cell_nb) == 0:
                 continue
@@ -117,43 +136,45 @@ class PointCloud:
         with laspy.open(path, "w", header=self.header) as f:
             f.write_points(self.las.points)
 
+    def _init_index(self):
+        if self.index is None:
+            # noinspection PyUnresolvedReferences
+            self.index = sklearn.neighbors.KDTree(self.las.xyz)
+
 
 # noinspection PyTypeChecker
 def merge(
-    in_paths: Sequence[str | PathLike],
-    out_path: str | PathLike,
+    ipaths: Sequence[str | PathLike],
+    opath: str | PathLike,
     crop: Optional[BoundingBoxLike] = None,
-    overlapping_tiles: bool = False,
+    remove_duplicates: bool = False,
 ) -> None:
-    out_header = _init_out_file(in_paths, out_path)
+    out_header = _init_out_file(ipaths[0], opath)
 
-    with laspy.open(out_path, "a") as f:
+    with laspy.open(opath, "a") as f:
         # Initialize the output header.
         # NOTE: The initial bounding box of the merged output should be defined
         #       around its region so that it can grow correctly.
         f.header.mins = out_header.mins
         f.header.maxs = out_header.maxs
 
-        visited_bboxes = []
-        for path in in_paths:
+        for path in ipaths:
             tile = PointCloud(path)
             # Translate the local coordinate system of the point cloud that of the
             # output file.
             tile.las.change_scaling(f.header.scales, f.header.offsets)
             if crop is not None:
                 tile.crop(crop)
-            if overlapping_tiles:
-                _remove_overlap(tile, visited_bboxes)
             f.append_points(tile.points)
+    if remove_duplicates:
+        PointCloud(opath).remove_duplicates().save(opath)
 
 
-def _init_out_file(
-    in_paths: Sequence[str | PathLike], out_path: str | PathLike
-) -> laspy.LasHeader:
-    with laspy.open(in_paths[0]) as f:
-        out_header = f.header
-        laspy.open(out_path, "w", header=out_header).close()
-    return out_header
+def _init_out_file(ipath: str | PathLike, opath: str | PathLike) -> laspy.LasHeader:
+    with laspy.open(ipath) as f:
+        ohead = f.header
+        laspy.open(opath, "w", header=ohead).close()
+    return ohead
 
 
 # TODO: Consider adopting shapely.Polygon to avoid spaghetti code.
@@ -170,6 +191,19 @@ def _remove_overlap(tile: PointCloud, visited_bboxes: list[BoundingBoxLike]) -> 
 
 if __name__ == "__main__":
     import config
+    import utils
 
     config.config()
-    pc = PointCloud("bk.laz").rasterize("z", size=0.25)["z"].save("bk.z.tiff")
+    obj_id = "9-284-556"
+    surfs = utils.geom.buffer(utils.geom.read_surfaces(obj_id))
+    paths = [
+        "C:/Documents/RoofSense/data/temp/37EN1_15.LAZ",
+        "C:/Documents/RoofSense/data/temp/37EN1_20.LAZ",
+        "C:/Documents/RoofSense/data/temp/37EN2_11.LAZ",
+        "C:/Documents/RoofSense/data/temp/37EN2_16.LAZ",
+    ]
+    # for i, perm in enumerate(itertools.permutations(paths)):
+    #     print(f"{i}/{24}", perm)
+    merge(
+        paths, "test.merged.gtxm.laz", crop=surfs.total_bounds, remove_duplicates=True
+    )
