f7200d29e31bdd9d56075dd2c4f31e8f079cfb4b
Dimitris Mantas <dimitris.mantas@outlook.com>
2024-02-07 21:19:55 +0100

    add mask geo referencing

--- utils/iris.py
+++ utils/iris.py
@@ -2,12 +2,15 @@ from __future__ import annotations
 
 import json
 import pathlib
+from os import PathLike
+
+import rasterio.io
 
 import config
 import utils.file
 
 
-def write_cfg():
+def generate_config_f():
     with pathlib.Path(config.var("IRIS_BASE_CFG")).open() as f:
         cfg = json.load(f)
     out_path = (
@@ -22,3 +25,20 @@ def write_cfg():
     cfg["segmentation"]["mask_area"] = [0, 0, *img_shp]
     with pathlib.Path(out_path).open("w") as f:
         json.dump(cfg, f)
+
+
+def georeference_masks(root_dir: str | PathLike) -> None:
+    img_paths = sorted(pathlib.Path(root_dir).joinpath("imgs").glob("*.tif"))
+    msk_paths = sorted(pathlib.Path(root_dir).joinpath("msks").glob("*.tif"))
+    for img_path, msk_path in zip(img_paths, msk_paths):
+        img: rasterio.io.DatasetReader
+        with rasterio.open(img_path) as img:
+            img_crs = img.crs
+            img_trf = img.transform
+        msk: rasterio.io.DatasetWriter
+        with rasterio.open(msk_path) as msk:
+            msk_data = msk.read()
+            msk_meta = msk.meta
+        msk_meta.update(crs=img_crs, transform=img_trf)
+        with rasterio.open(msk_path, "w", **msk_meta) as out:
+            out.write(msk_data)
