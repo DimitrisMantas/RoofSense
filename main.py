24935e4bcebed4555c5ecd5d8c7fbd4eb9eeb2c3
Dimitris Mantas <dimitris.mantas@outlook.com>
2024-02-10 03:47:03 +0100

    revert main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import warnings
 from os import PathLike
 
 import config
@@ -9,11 +8,8 @@ import utils.iris
 
 
 # noinspection PyUnusedLocal
-def generate_training_data(size: int = 10, background_cutoff: float = 0.5) -> None:
-    """
-    The entry point for:
-        roofsense --gen-training-data <size> --background-cutoff <pct>
-    """
+def generate_pretraining_data(size: int = 10, background_cutoff: float = 0.5) -> None:
+    """Entry point for 'roofsense --sample <size>'."""
 
     # Initialize the program runtime.
     config.config(pretraining=True)
@@ -28,32 +24,12 @@ def generate_training_data(size: int = 10, background_cutoff: float = 0.5) -> No
     lidar_parser = preprocessing.parsers.LiDARParser()
 
     # Fake a random sample.
-    samples = preprocessing.sampler.BAG3DSampler().sample(size)
-    for i, sample in enumerate(samples, start=1):
-        print(f"Generating training data for tile {i}/{size}: {sample}...")
-
+    samples = ["9-284-556"]
+    for sample in samples:
         # Download the corresponding 3DBAG data.
         bag3d_downloader.download(sample)
         # Parse the data.
         bag3d_parser.parse(sample)
-
-        # Check if the spatial distribution of the buildings in the tile is erroneous
-        # (e.g., small disjoint building clusters ets.).
-        # NOTE: This ensures that only tiles with the minimal possible number of
-        # assets are processed in subsequent steps.
-        surfs = utils.geom.read_surfaces(sample)
-
-        surf_bbox = surfs.total_bounds
-        bbox_xlen = surf_bbox[2] - surf_bbox[0]
-        bbox_ylen = surf_bbox[3] - surf_bbox[1]
-        # TODO: Remove this check once the point cloud rasterisation process has been
-        #      optimised.
-        # NOTE: Erroneous tiles are defined as those whose axis-aligned bounding box
-        #       has at least one side
-        #       which is longer than the maximum side length of an AHN4 tile.
-        if bbox_xlen > 1250 or bbox_ylen > 1250:
-            warnings.warn("The tile is erroneous. Skipping...")
-            continue
 
         # Download the corresponding assets.
         asset_downloader.download(sample)
@@ -72,13 +48,10 @@ def generate_training_data(size: int = 10, background_cutoff: float = 0.5) -> No
 
 
 def train(root: str | PathLike) -> None:
-    """
-    The entry point for:
-        roofsense --train <cfg>
-    """
+    """Entry point for 'roofsense --train <root>'."""
 
     utils.iris.georeference_masks(root)
 
 
 if __name__ == "__main__":
-    generate_training_data()
+    generate_pretraining_data()
