d139115ac68930c0f0be9f1f298715a95de2df72
Dimitris Mantas <dimitris.mantas@outlook.com>
2024-02-10 04:47:07 +0100

    adjust bg cutoff to compensate for loss of buffers

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import utils.iris
 
 
 # noinspection PyUnusedLocal
-def generate_pretraining_data(size: int = 10, background_cutoff: float = 0.5) -> None:
+def generate_pretraining_data(size: int = 10, background_cutoff: float = 0.7) -> None:
     """
     Entry point for:
         roofsense --gen-pretrain-data <size> --bg-cutoff <pct>
