ddcc176aea4799e05e699b1c82ff13aa06212b9e
Dimitris Mantas <dimitris.mantas@outlook.com>
2024-03-19 21:42:03 +0200

    update asset downloader

--- preprocessing/downloaders/asset.py
+++ preprocessing/downloaders/asset.py
@@ -2,88 +2,109 @@ from __future__ import annotations
 
 import json
 import pathlib
-import urllib.parse
+from os import PathLike
+from typing import Any, Optional
 
 import geopandas as gpd
+import numpy as np
 import requests
-from typing_extensions import override
 
 import config
 import utils
-from preprocessing.downloaders.base import DataDownloader
-from utils.type import BoundingBoxLike
 
 
-class AssetDownloader(DataDownloader):
+class AssetDownloader:
     def __init__(self) -> None:
-        super().__init__()
-        self._index = gpd.read_file(config.env("ASSET_SHEET_INDEX"))
+        self._image_info_store = _InfoStore(config.env("IMAGE_SHEET_INDEX"))
+        self._lidar_info_store = _InfoStore(config.env("LIDAR_SHEET_INDEX"))
 
-    @override
-    def download(self, obj_id: str | BoundingBoxLike) -> None:
-        img_ids, ldr_ids = self._get_asset_ids(obj_id)
-        _write_asset_manifest(obj_id, img_ids, ldr_ids)
-        # TODO: Parallelize this operation.
-        _download_image_assets(img_ids)
-        _download_lidar_assets(ldr_ids)
+    def download(self, tile_id: str) -> None:
+        image_info = self._image_info_store.get_info(tile_id)
+        lidar_info = self._lidar_info_store.get_info(tile_id)
 
-    def _get_asset_ids(
-        self, obj_id: str | BoundingBoxLike
-    ) -> tuple[list[str], list[str]]:
-        surfs = utils.geom.read_surfaces(obj_id)
-        ids = self._index.overlay(surfs)
-        return (
-            ids[config.var("ASSET_INDEX_IMAGE_IDS")].unique().tolist(),
-            ids[config.var("ASSET_INDEX_LIDAR_IDS")].unique().tolist(),
-        )
+        manifest = self._gen_manifest(tile_id, image_info, lidar_info)
 
+        urls = manifest["image"]["urls"] + manifest["lidar"]["urls"]
+        dst_filepaths = [
+            pathlib.Path(config.env("TEMP_DIR")) / pathlib.Path(url).name
+            for url in urls
+        ]
 
-def _write_asset_manifest(obj_id: str, img_ids: list[str], ldr_ids: list[str]) -> None:
-    # TODO: Create this path using a utility function ro avoid code duplication.
-    out_path = (
-        f"{config.var('TEMP_DIR')}"
-        f"{obj_id}"
-        f"{config.var('ASSET_MANIFEST_EXTENSION')}"
-        f"{config.var('JSON')}"
-    )
-    if utils.file.exists(out_path):
-        return
-    manifest = {
-        config.var("ASSET_MANIFEST_IMAGE_IDS"): [
-            f"{img_id}{config.var('TIFF')}" for img_id in img_ids
+        with requests.Session() as session:
+            utils.file.ThreadedFileDownloader(
+                urls, filenames=dst_filepaths, session=session
+            ).download()
+
+    def _gen_manifest(
+        self,
+        tile_id: str,
+        image_info: tuple[
+            np.ndarray[tuple[Any,], np.dtype[np.object_]],
+            np.ndarray[tuple[Any,], np.dtype[np.object_]],
         ],
-        config.var("ASSET_MANIFEST_LIDAR_IDS"): [
-            f"{ldr_id}{config.var('LAZ')}" for ldr_id in ldr_ids
+        lidar_info: tuple[
+            np.ndarray[tuple[Any,], np.dtype[np.object_]],
+            np.ndarray[tuple[Any,], np.dtype[np.object_]],
         ],
-    }
-    with pathlib.Path(out_path).open("w") as f:
-        json.dump(manifest, f)
+    ) -> Optional[dict[str, dict[str, list[str]]]]:
+        # TODO: Think of a better way to compile paths from individual user-defined
+        #  parameters.
+        dst_filepath = pathlib.Path(
+            config.env("TEMP_DIR")
+        ) / f'{tile_id}{config.var("ASSET_MANIFEST_EXTENSION")}{config.var("JSON")}'
+
+        if dst_filepath.is_file():
+            with dst_filepath.open() as src:
+                return json.load(src)
+
+        manifest = {
+            "image": self._image_info_store.gen_manifest(image_info),
+            "lidar": self._lidar_info_store.gen_manifest(lidar_info),
+        }
+
+        with dst_filepath.open("w") as dst:
+            json.dump(manifest, dst)
+
+        return manifest
 
 
-def _download_image_assets(ids: list[str]) -> None:
-    urls = [
-        f"{config.var('BASE_IMAGE_DATA_URL')}{img_tp}_{img_id}{config.var('TIFF')}"
-        for img_tp in [config.var("CIR_IDENTIFIER"), config.var("RGB_IDENTIFIER")]
-        for img_id in ids
-    ]
-    out_paths = _get_output_paths(urls)
-    with requests.Session() as s:
-        utils.file.ThreadedFileDownloader(urls, out_paths, session=s).download()
+# TODO: Check whether this is an appropriate name given the purpose of this class.
+class _InfoStore:
+    def __init__(self, index_path: str | bytes | PathLike) -> None:
+        self._index: gpd.GeoDataFrame = gpd.read_file(index_path)
+
+    def get_info(
+        self, tile_id: str
+    ) -> tuple[
+        np.ndarray[tuple[Any,], np.dtype[np.object_]],
+        np.ndarray[tuple[Any,], np.dtype[np.object_]],
+    ]:
+        # NOTE: This operation implicitly requires that the roof surfaces have
+        # already been parsed.
+        surfs = utils.geom.read_surfaces(tile_id)
+
+        hits = self._index.overlay(surfs)
+
+        # NOTE: The matches initially contain duplicate values because each surface
+        # is separately intersected with the index.
+        return hits.tid.unique(), hits.url.unique()
+
+    @staticmethod
+    def gen_manifest(
+        info: tuple[
+            np.ndarray[tuple[Any,], np.dtype[np.object_]],
+            np.ndarray[tuple[Any,], np.dtype[np.object_]],
+        ],
+    ) -> dict[str, list[str]]:
+        manifest = {
+            "tids": info[0].tolist(),
+            "urls": info[1].tolist(),
+        }
+
+        return manifest
 
 
-def _download_lidar_assets(ids: list[str]) -> None:
-    urls = [
-        f"{config.var('BASE_LIDAR_DATA_URL')}{ldr_id}{config.var('LAZ')}"
-        for ldr_id in ids
-    ]
-    out_paths = _get_output_paths(urls)
-    with requests.Session() as s:
-        utils.file.ThreadedFileDownloader(urls, out_paths, session=s).download()
+if __name__ == "__main__":
+    config.config()
 
-
-# TODO: Move this function to a more appropriate module.
-def _get_output_paths(urls: list[str]) -> list[str]:
-    return [
-        f"{config.var('TEMP_DIR')}{urllib.parse.urlparse(url).path.rsplit('/')[-1]}"
-        for url in urls
-    ]
+    AssetDownloader().download("9-284-556")
