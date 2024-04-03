3daf8d22342bff939ebae31c852ec744185b3e42
Dimitris Mantas <dimitris.mantas@outlook.com>
2024-04-03 23:32:08 +0200

    update point cloud engine

--- preprocessing/parsers/utils/pcloud.py
+++ preprocessing/parsers/utils/pcloud.py
@@ -1,17 +1,16 @@
 from __future__ import annotations
 
-import json
-from collections.abc import Iterable, Sequence
+from collections.abc import Sequence, Iterable
+from functools import cache
 from io import BytesIO
 from os import PathLike
-from typing import Any, Literal, Optional, Self
+from typing import Optional, Self
 
 import laspy
 import numpy as np
 import polars as pl
 import rasterio
-import startinpy
-from tqdm import tqdm
+import scipy as sp
 
 from preprocessing.parsers.utils import raster
 from utils.type import BoundingBoxLike
@@ -24,11 +23,7 @@ class PointCloud:
     formats."""
 
     def __init__(
-        self,
-        filepath: str | bytes | BytesIO,
-        init_index: bool = False,
-        duplicate_predicate: Literal["First", "Highest", "Last", "Lowest"] = "Highest",
-        duplicate_tolerance: float = 0.02,
+        self, filepath: str | bytes | BytesIO, init_index: bool = False
     ) -> None:
         """Initialize the point cloud.
 
@@ -39,34 +34,25 @@ class PointCloud:
         index is created automatically, if one does not already exist, to support all
         operations whose performance would significantly benefit from optimized
         spatial access methods.
-
-        :param duplicate_predicate: The duplicate point handling strategy used by the spatial index of the point cloud. Given a group of duplicate points in  :math:`\mathcal{R}^{2}`, as specified by
-
-        :param duplicate_tolerance:
         """
         # Initialize the cloud.
         with laspy.open(filepath) as src:
             self._data = src.read()
 
         # Initialize the index.
-        self._index = (
-            Index(
-                self,
-                duplicate_predicate=duplicate_predicate,
-                duplicate_tolerance=duplicate_tolerance,
-            )
-            if init_index
-            else None
-        )
+        self._index = Index(self) if init_index else None
 
-        # NOTE: The index configuration must be persisted] for it to be able to be
-        #       initialized at a later time.
-        self._duplicate_predicate = duplicate_predicate
-        self._duplicate_tolerance = duplicate_tolerance
+    # TODO: Document this method.
+    def __getattr__(self, item):
+        return self._data.item
+
+    # TODO: Document this method.
+    def __getitem__(self, item):
+        return self._data[item]
 
     def __len__(self) -> int:
         """Get the number of points in the point cloud."""
-        return len(self.data)
+        return len(self._data)
 
     @property
     def bbox(self) -> BoundingBoxLike:
@@ -75,16 +61,6 @@ class PointCloud:
         extent of the point cloud in the length unit of its coordinate reference
         system."""
         return *self.header.mins[:2], *self.header.maxs[:2]
-
-    @property
-    def data(self) -> laspy.LasData:
-        """Get the header, point, variable, and extended variable length records of
-        the file containing the point cloud.
-
-        .. note::
-            See :class:`laspy.LasData` for more information.
-        """
-        return self._data
 
     @property
     def index(self) -> Optional[Index]:
@@ -101,7 +77,7 @@ class PointCloud:
         .. note::
             See :class:`laspy.LasHeader` for more information.
         """
-        return self.data.header
+        return self._data.header
 
     @property
     def points(self) -> laspy.PackedPointRecord:
@@ -110,7 +86,7 @@ class PointCloud:
         .. note::
         See :class:`laspy.PackedPointRecord` for more information.
         """
-        return self.data.points
+        return self._data.points
 
     def crop(self, bbox: BoundingBoxLike) -> Self:
         """Crop the point cloud.
@@ -137,36 +113,25 @@ class PointCloud:
         xmax = int((bbox[2] - self.header.x_offset) / self.header.x_scale)
         ymax = int((bbox[3] - self.header.y_offset) / self.header.y_scale)
 
-        self.data.points = self.points[
+        self._data.points = self.points[
             np.logical_and(
-                np.logical_and(xmin <= self.points["X"], self.points["X"] <= xmax),
-                np.logical_and(ymin <= self.points["Y"], self.points["Y"] <= ymax),
+                np.logical_and(xmin <= self.X, self.X <= xmax),
+                np.logical_and(ymin <= self.Y, self.Y <= ymax),
             )
         ]
 
         # Invalidate the index.
         if self.index is not None:
-            self._index = Index(
-                self,
-                duplicate_predicate=self.index.duplicates_handling,
-                duplicate_tolerance=self.index.snap_tolerance,
-            )
+            self._index = Index(self)
 
         return self
 
     def normals(self) -> Self:
         # Initialize the index.
-        # TODO: Factor this block out to a private method.
         if self.index is None:
-            self._index = Index(
-                self,
-                duplicate_predicate=self._duplicate_predicate,
-                duplicate_tolerance=self._duplicate_tolerance,
-            )
+            self._index = Index(self)
 
-        self.index.normals()
-
-        return self
+        raise NotImplementedError
 
     def rasterize(
         self,
@@ -175,182 +140,157 @@ class PointCloud:
         bbox: Optional[BoundingBoxLike] = None,
         meta: Optional[rasterio.profiles.Profile] = None,
     ) -> raster.Raster:
-        """Rasterize a single point dimension from the standard, variable, or extended variable length point record of the point cloud.
+        """Rasterize a single point dimension from the standard, variable,
+        or extended variable length point record of the point cloud.
 
         .. note::
-            The dimension value at the cell centers of the resulting raster is interpolated using Laplace interpolation. Because this approach does not support extrapolation, any no-data cells remaining after the primary interpolation process are automatically identified and filled with ``rasterio.fill.fillnodata()``.
+            The dimension value at the cell centers of the resulting raster is
+            interpolated using Laplace interpolation. Because this approach does not
+            support extrapolation, any no-data cells remaining after the primary
+            interpolation process are automatically identified and filled with
+            ``rasterio.fill.fillnodata()``.
 
-        :param dim: The point dimension to rasterize. See `Point Records <https://laspy.readthedocs.io/en/latest/intro.html#point-records>`_ for more information.
+        :param dim: The point dimension to rasterize. See `Point Records
+        <https://laspy.readthedocs.io/en/latest/intro.html#point-records>`_ for more
+        information.
 
-        :param res: The resolution of the resulting raster in the coordinate reference system of the point cloud.
+        :param res: The resolution of the resulting raster in the coordinate
+        reference system of the point cloud.
 
-        :param bbox: The two-dimensional, coordinate interleaved, axis-aligned bounding box, :math:`[x_{min}, y_{min}, x_{max}, y_{max}]`, specifying the spatial extent of the resulting raster in the coordinate reference system of the point cloud. Defaults to ``PointCloud.bbox`` if left unspecified.
+        :param bbox: The two-dimensional, coordinate interleaved, axis-aligned
+        bounding box, :math:`[x_{min}, y_{min}, x_{max}, y_{max}]`, specifying the
+        spatial extent of the resulting raster in the coordinate reference system of
+        the point cloud. Defaults to ``PointCloud.bbox`` if left unspecified.
 
-        :param meta: The `profile <https://rasterio.readthedocs.io/en/stable/topics/profiles.html>`_ of the resulting raster. Defaults to :class:`raster.DefaultProfile` if left unspecified.
+        :param meta: The `profile <https://rasterio.readthedocs.io/en/stable/topics
+        /profiles.html>`_ of the resulting raster. Defaults to
+        :class:`raster.DefaultProfile` if left unspecified.
 
         :return: The resulting raster.
         """
-        # Resolve the input.
-        _bbox = bbox if bbox is not None else self.bbox
+        # Disambiguate the input.
+        bbox = bbox if bbox is not None else self.bbox
 
         # Initialize the index.
         # TODO: Factor this block out to a private method.
         if self.index is None:
-            self._index = Index(
-                self,
-                duplicate_predicate=self._duplicate_predicate,
-                duplicate_tolerance=self._duplicate_tolerance,
-            )
-        self.index.update(self.points[dim].scaled_array(), dim=dim)
+            self._index = Index(self)
 
-        # Generate the output.
-        out = raster.Raster(res, bbox=_bbox, meta=meta)
-        with tqdm(desc="Rasterization", total=len(out), unit="cells") as pbar:
-            for row in range(out.height):
-                y = _bbox[3] - res * (row + 0.5)
-                for col in range(out.width):
-                    x = _bbox[0] + res * (col + 0.5)
-                    out[row, col] = self._index.interpolate([x, y])
-                    pbar.update()
-
-        return out
+        raise NotImplementedError
 
     def remove_duplicates(self) -> Self:
-        pts = np.vstack(
-            [
-                self.points["X"],
-                self.points["Y"],
-                self.points["Z"],
-            ]
-        ).transpose()
-
+        # Gather the point records.
         # NOTE: The unique element filter provided by NumPy is inefficient.
         #       See https://github.com/numpy/numpy/issues/11136 for more information.
-        #       In addition,
-        #       this approach does not disturb the spatial coherence of the point cloud
-        #       and is multithreaded.
-        pts = pl.DataFrame(pts, schema=["X", "Y", "Z"])
-        pts = pts.with_row_index()
+        recs = pl.DataFrame(
+            np.vstack(
+                [
+                    self.X,
+                    self.Y,
+                    self.Z,
+                ]
+            ).transpose(),
+            schema=["X", "Y", "Z"],
+        )
+        recs = recs.with_row_index()
 
-        # Sort the point records by their elevation.
+        # Sort the point records.
         # NOTE: This ensures that only contextually irrelevant points are discarded.
-        pts = pts.sort("Z")
+        recs = recs.sort("Z")
 
-        self.data.points = self.data.points[
-            pts.unique(subset=["X", "Y"], keep="last")["index"].sort().to_numpy()
+        # Filter the point records.
+        # NOTE: The unique point indices must be sorted so as to not disturb the
+        #       spatial coherence of the point cloud.
+        self._data.points = self.data.points[
+            recs.unique(subset=["X", "Y"], keep="last")["index"].sort().to_numpy()
         ]
 
         return self
 
     def save(self, filepath: str | bytes | BytesIO) -> None:
-        """Save the point cloud to  a `LASer (LAS)
+        """Save the point cloud to a `LASer
         <https://www.asprs.org/divisions-committees/lidar-division/laser-las-file
-        -format -exchange-activities>`_ or `LASzip <https://rapidlasso.de/laszip/>`_
+        -format-exchange-activities>`_ and `LASzip <https://rapidlasso.de/laszip/>`_
         file.
 
-        :param filepath: The path to the resulting file.
+        :param filepath: The path to the output file.
         """
         with laspy.open(filepath, mode="w", header=self.header) as dst:
-            dst.write_points(self.data.points)
+            dst.write_points(self._data.points)
 
 
 class Index:
-    def __init__(
-        self,
-        pt_cloud: PointCloud,
-        duplicate_predicate: Literal["First", "Highest", "Last", "Lowest"],
-        duplicate_tolerance: float,
-    ) -> None:
-        # The underlying DT.
-        self._dt = startinpy.DT(extra_attributes=True)
-        self._dt.duplicates_handling = duplicate_predicate
-        self._dt.snap_tolerance = duplicate_tolerance
+    def __init__(self, pc: PointCloud, workers: int = -1, **kwargs) -> None:
+        self.workers = workers
 
-        # The point dimension currently stored in the index.
-        self._pt_dim = "z"
-
-        for i, pt in tqdm(
-            enumerate(pt_cloud.data.xyz),
-            desc="Index Initialization",
-            total=len(pt_cloud),
-            unit="points",
-        ):
-            self._dt.insert_one_pt(pt, pid=i)
-
-        # Allow the point dimension to be overwritten regardless of the currently
-        # active duplicate handling method.
-        self._dt.duplicates_handling = "Last"
+        self._struct = sp.spatial.KDTree(
+            np.vstack([pc.data.x, pc.data.y]).transpose(), **kwargs
+        )
 
     def __len__(self) -> int:
-        return self._dt.number_of_vertices()
+        return self._struct.n
 
-    @property
-    def duplicates_handling(self) -> Literal["First", "Highest", "Last", "Lowest"]:
-        return self._dt.duplicates_handling
-
-    @duplicates_handling.setter
-    def duplicates_handling(
-        self, method: Literal["First", "Highest", "Last", "Lowest"]
-    ) -> None:
-        self._dt.duplicates_handling = method
-
-    @property
-    def snap_tolerance(self) -> float:
-        return self._dt.snap_tolerance
-
-    @snap_tolerance.setter
-    def snap_tolerance(self, tol: float) -> None:
-        self._dt.snap_tolerance = tol
-
-    @property
-    def point_dim(self) -> Optional[str]:
-        return self._pt_dim
-
-    @property
-    def point_map(self):
-        return self._dt.attribute("pid")[1:].astype(int)
-
-    def interpolate(
-        self,
-        xy: Iterable[float],
-        method: Literal["IDW", "Laplace", "NN", "NNI", "TIN"] = "Laplace",
-    ) -> float:
-        return self._dt.interpolate({"method": method}, [xy])
+    def interpolate(self):
+        raise NotImplementedError
 
     def normals(self):
-        def normal_triangle(tr, pts):
-            v0 = pts[tr[1]] - pts[tr[0]]
-            v1 = pts[tr[2]] - pts[tr[0]]
-            n = np.cross(v0, v1)
-            l = np.linalg.norm(n)
-            return n / l
+        raise NotImplementedError
 
-        pts = self._dt.points
-        for pt_id in tqdm(range(1, len(pts))):
-            incident_triangles = self._dt.incident_triangles_to_vertex(pt_id)
-            incident_normals = np.empty([incident_triangles.shape[0], 3])
-            for tri_id, tri_pt_ids in enumerate(incident_triangles):
-                if self._dt.is_finite(tri_pt_ids):
-                    tri_normal = normal_triangle(tri_pt_ids, pts)
-                    incident_normals[tri_id] = tri_normal
-            pt_normal = np.average(incident_normals, axis=0)
-            pt_normal = pt_normal / np.linalg.norm(pt_normal)
-            s = json.dumps({"nx": pt_normal[0], "ny": pt_normal[1], "nz": pt_normal[2]})
-            self._dt.set_vertex_attributes(pt_id, s)
+    # TODO: Add support for non-hashable parameters.
+    # TODO: Harmonize the output data formats.
+    # noinspection PyProtectedMember
+    @cache
+    def query(
+        self,
+        x: np._typing._array_like._ArrayLikeFloat_co,
+        k: int | Iterable[int] | None = None,
+        r: float | Iterable[float] | None = None,
+        **kwargs,
+    ):
+        common_args = {"x": x, "workers": self.workers}
+        if k is not None and r is None:
+            distances, neighbors = self._struct.query(k=k, **common_args, **kwargs)
+        elif k is None and r is not None:
+            neighbors, distances = self._query_radius(r=r, **common_args, **kwargs)
+        else:
+            raise ValueError(
+                f"Could not disambiguate query type. Found non-null values for both {k!r} and {r!r}."
+            )
 
-    def update(self, z: np.ndarray[tuple[Any,], np.dtype[np.number]], dim: str) -> None:
-        if dim == self.point_dim:
-            return
+        return neighbors, distances
 
-        self._pt_dim = dim
+    # noinspection PyProtectedMember
+    def _query_radius(
+        self,
+        x: np._typing._array_like._ArrayLikeFloat_co,
+        r: float | Iterable[float],
+        **kwargs,
+    ):
+        x = np.asarray(x)
 
-        for i, pid in tqdm(
-            enumerate(self.point_map),
-            desc="Index Update",
-            total=len(self),
-            unit="points",
-        ):
-            self._dt.update_vertex_z_value(i, z[pid])
+        # Coerce the array to 2D.
+        if x.ndim == 1:
+            x = x[None, :]
+
+        # Find the neighbors.
+        neighbors = self._struct.query_ball_point(
+            x,
+            r=r,
+            # NOTE: Multi-point queries are sorted by index, which is not necessary.
+            return_sorted=False,
+            **kwargs,
+        )
+
+        # Find the distances.
+        distances = np.empty_like(neighbors)
+        for i, (cell, neighbor) in enumerate(zip(x, neighbors)):
+            # NOTE: Distance subarrays must be one-dimensional to match the neighbor
+            # array.
+            distances[i] = sp.spatial.distance.cdist(
+                cell[None, :], self._struct.data[neighbor]
+            ).ravel()
+
+        return neighbors, distances
 
 
 def merge(
@@ -376,9 +316,3 @@ def merge(
     if rem_dpls:
         opc.remove_duplicates()
     opc.save(opath)
-
-
-if __name__ == "__main__":
-    pc = PointCloud("small.laz")
-    pc.normals()
-    x = pc.index._dt.attribute("nx")
