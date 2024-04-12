40ab13a36faad101a2d2ea5e7b644dd75280183e
Dimitris Mantas <dimitris.mantas@outlook.com>
2024-04-12 09:14:19 +0200

    add sampler

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import config
 import preprocessing
 
 
-# noinspection PyUnusedLocal
-def generate_pretraining_data(size: int = 10, background_cutoff: float = 0.6) -> None:
+def generate_pretraining_data(size: int = 10, background_cutoff: float = 0.5) -> None:
     """
     Entry point for:
         roofsense --gen-pretrain-data <size> --bg-cutoff <pct>
@@ -23,7 +22,7 @@ def generate_pretraining_data(size: int = 10, background_cutoff: float = 0.6) ->
     lidar_parser = preprocessing.parsers.LiDARParser()
 
     # Fake a random sample.
-    samples = ["9-284-556"]
+    samples = preprocessing.sampler.BAG3DSampler().sample(size)
     for sample in samples:
         # Download the corresponding 3DBAG data.
         bag3d_downloader.download(sample)
