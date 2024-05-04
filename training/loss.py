17e5cbd13b5902373ebed5913fd5d8d0f1cfdf4c
Dimitris Mantas <dimitris.mantas@outlook.com>
2024-05-04 16:25:30 +0300

    log iou

--- training/loss.py
+++ training/loss.py
@@ -12,29 +12,30 @@ class CrossEntropyJaccardLoss(_WeightedLoss):
         # Input
         num_classes: int,
         ignore_index: int = -100,
-        # CE
         class_weights: Tensor | None = None,
-        reduction: Literal["mean", "sum"] = "mean",
-        label_smoothing: float = 0,
+        # CE
+        nll_reduction: Literal["mean", "sum"] = "mean",
+        nll_smoothing: float = 0,
         # IoU
-        log_iou: bool = False,
+        iou_logarithmic: bool = True,
+        iou_smoothing: float = 0,
         # Output
         nll_activation: float = 1,
         iou_activation: float = 1,
     ):
-        super().__init__(class_weights, reduction=reduction)
+        super().__init__(class_weights, reduction=nll_reduction)
 
         self.nll = CrossEntropyLoss(
             class_weights,
             ignore_index=ignore_index,
-            reduction=reduction,
-            label_smoothing=label_smoothing,
+            reduction=nll_reduction,
+            label_smoothing=nll_smoothing,
         )
         self.iou = JaccardLoss(
             "multiclass",
             classes=[i for i in range(num_classes) if i != ignore_index],
-            log_loss=log_iou,
-            smooth=label_smoothing,
+            log_loss=iou_logarithmic,
+            smooth=iou_smoothing,
         )
 
         self.nll_activation = nll_activation
