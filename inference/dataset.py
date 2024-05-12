891d6cce6b78f7e428970a37dbceed84778803e7
Dimitris Mantas <dimitris.mantas@outlook.com>
2024-05-12 23:53:58 +0200

    add inference code

---
+++ inference/dataset.py
@@ -0,0 +1,52 @@
+from collections.abc import Callable, Sequence
+from typing import Any
+
+from torchgeo.datasets import RasterDataset
+
+
+class InferenceDataset(RasterDataset):
+    # GeoDataset
+    filename_glob = "*.stack.tif"
+    # RasterDataset
+    # TODO: Fill these fields in.
+    all_bands: list[str] = []
+    rgb_bands: list[str] = []
+    cmap: dict[int, tuple[int, int, int, int]] = {}
+
+    def __init__(
+        self,
+        # TODO: Provide the tile 3D or its location?
+        root: str,
+        tile_id: str | None = None,
+        download: bool = False,
+        checksum: bool = False,
+        bands: Sequence[str] | None = None,
+        transforms: Callable[[dict[str, Any]], dict[str, Any]] | None = None,
+        cache: bool = True,
+    ) -> None:
+        self.root = root
+
+        # TODO: Support multiple stacks, just process them one at a time.
+        if tile_id is not None:
+            raise NotImplementedError("The root must contain a single stack.")
+
+        # TODO: Build the stack if it does not already exist.
+        if download or checksum:
+            raise NotImplementedError("The raster stack must already exist.")
+
+        super().__init__(root, bands=bands, transforms=transforms, cache=cache)
+
+        base_err_msg = (
+            f"Expected a single stack at location: {self.root!r},"
+            f" "
+            f"but got {len(self)} instead."
+        )
+        if len(self) == 0:
+            raise ValueError(base_err_msg + " " + "Empty dataset root path.")
+        elif len(self) > 1:
+            raise ValueError(base_err_msg + " " + "Invalid dataset root path.")
+
+
+if __name__ == "__main__":
+    dataset = InferenceDataset("../dataset/infer")
+    x = 1
---
+++ inference/task.py
@@ -0,0 +1,165 @@
+import os
+
+import lightning
+import numpy as np
+import rasterio
+import torch
+import torch.utils.data
+import torchgeo.datasets
+import torchgeo.samplers
+import torchgeo.transforms
+import tqdm
+
+import common.augmentations
+import inference.datamodule
+import inference.dataset
+import training.task
+
+
+# TODO: Add support for changing the current model and tile.
+class InferenceEngine:
+    def __init__(
+        self,
+        data_root: str,
+        model_ckpt: str,
+        seed: int | None = None,
+        tf32: bool = True,
+    ) -> None:
+        lightning.pytorch.seed_everything(seed, workers=True)
+
+        if tf32:
+            torch.set_float32_matmul_precision("high")
+            torch.backends.cudnn.allow_tf32 = True
+
+        self.dataset = inference.dataset.InferenceDataset(data_root)
+        self.augmet = torchgeo.transforms.AugmentationSequential(
+            common.augmentations.MinMaxScaling(
+                # TODO: Expose these parameters in the initializer.
+                mins=torch.tensor([0, 0, 0, 0, 0]),
+                maxs=torch.tensor([255, 255, 255, 1, 90]),
+            ),
+            data_keys=["image"],
+        )
+
+        self.model: training.task.TrainingTask = (
+            training.task.TrainingTask.load_from_checkpoint(model_ckpt)
+        )
+        self.model.eval()
+
+        self.sampler = torchgeo.samplers.GridGeoSampler(
+            self.dataset,
+            # TODO: Expose these parameters in the initializer.
+            size=512,
+            stride=256,
+        )
+        self.loader = torch.utils.data.DataLoader(
+            self.dataset,
+            # TODO: Expose these parameters in the initializer.
+            batch_size=1,
+            sampler=self.sampler,
+            # num_workers=max(os.cpu_count() // 2, 1),
+            num_workers=1,
+            collate_fn=torchgeo.datasets.stack_samples,
+            pin_memory=True,
+            generator=torch.Generator().manual_seed(0),
+            persistent_workers=True,
+        )
+
+        self.trainer = lightning.Trainer(benchmark=True)
+
+        # TODO: Expose these parameters in the initializer.
+        self.size = torchgeo.samplers.utils._to_tuple(512)
+        self.strd = torchgeo.samplers.utils._to_tuple(256)
+
+        self.size = (self.size[0] * self.dataset.res, self.size[1] * self.dataset.res)
+        self.strd = (self.strd[0] * self.dataset.res, self.strd[1] * self.dataset.res)
+
+        self.rows, self.cols = torchgeo.samplers.utils.tile_to_chips(
+            self.dataset.bounds, self.size, self.strd
+        )
+
+        self.width = int(
+            (self.dataset.bounds.maxx - self.dataset.bounds.minx) / self.dataset.res
+        )
+        self.height = int(
+            (self.dataset.bounds.maxy - self.dataset.bounds.miny) / self.dataset.res
+        )
+        # TODO: Expose these parameters in the initializer.
+        self.buffer = np.zeros(
+            (256 * (self.rows + 1), 256 * (self.cols + 1)), dtype=np.uint8
+        )
+
+    def run(self):
+        img_idx = 0
+        for batch in tqdm.tqdm(
+            self.loader,
+            total=len(self.loader),
+            desc=self.__class__.__name__.replace("Engine", ""),
+        ):
+            # NOTE: Augmentations are performed following the batch sampling step to
+            # avoid broadcasting them into 5D, which is the default behavior of
+            # MinMaxScaling.
+            batch: dict[
+                str,
+                list[rasterio.crs.CRS]
+                | list[torchgeo.datasets.BoundingBox]
+                | torch.Tensor,
+            ] = self.augmet(batch)
+            images: torch.Tensor = batch["image"].to(self.model.device)
+
+            with torch.inference_mode():
+                preds: torch.Tensor = (
+                    self.model(images).softmax(dim=1).argmax(dim=1).cpu().numpy()
+                )
+
+            for i in range(images.shape[0]):
+                row_idx, col_idx = divmod(
+                    img_idx,
+                    self.cols
+                )
+                # NOTE: The chip sampler starts at the southeast corner of the stack.
+                row_off, col_off = len(self.buffer) - row_idx * 256, col_idx * 256
+
+                self.buffer[
+                    row_off - 512 : row_off,
+                    col_off : col_off + 512
+                ] = preds[i]
+
+                img_idx += 1
+
+        dst: rasterio.io.DatasetWriter
+        with rasterio.open(
+            "pred.tif",
+            mode="w",
+            width=self.width,
+            height=self.height,
+            count=1,
+            crs=self.dataset.crs,
+            transform=rasterio.transform.from_origin(
+                west=self.dataset.bounds.minx,
+                north=self.dataset.bounds.maxy,
+                xsize=self.dataset.res,
+                ysize=self.dataset.res,
+            ),
+            dtype=np.uint8,
+            nodata=0,
+            tiled=True,
+            blockxsize=512,
+            blockysize=512,
+            compress="DEFLATE",
+            num_threads=os.cpu_count(),
+        ) as dst:
+            dst.write(
+                # Trim the buffer to the stack dimensions.
+                self.buffer[
+                    len(self.buffer) - self.height : len(self.buffer), : self.width
+                ],
+                indexes=1,
+            )
+
+
+if __name__ == "__main__":
+    engine = InferenceEngine(
+        data_root="../dataset/infer", model_ckpt="../logs/RoofSense/best-v0.ckpt"
+    )
+    engine.run()
