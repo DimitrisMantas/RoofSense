a67207a580c9b304297a25950a2228e9ebfc94a7
Dimitris Mantas <dimitris.mantas@outlook.com>
2024-05-26 01:50:56 +0200

    add callbacks

--- training/engine.py
+++ training/engine.py
@@ -4,9 +4,7 @@ import lightning.pytorch
 import numpy as np
 import torch
 from lightning import Trainer
-from lightning.pytorch.callbacks import (EarlyStopping,
-                                         LearningRateMonitor,
-                                         ModelCheckpoint, )
+from lightning.pytorch.callbacks import LearningRateMonitor
 from lightning.pytorch.loggers import TensorBoardLogger
 
 from training.datamodule import TrainingDataModule
@@ -39,29 +37,31 @@ if __name__ == "__main__":
         },
     )
 
-    datamodule = TrainingDataModule(
-        root="../dataset/temp", batch_size=8, num_workers=8
-    )
+    datamodule = TrainingDataModule(root="../dataset/temp")
 
     trainer = Trainer(
         logger=TensorBoardLogger(save_dir="../logs/RoofSense"),
         callbacks=[
-            ModelCheckpoint(
+            lightning.pytorch.callbacks.EarlyStopping(
+                monitor="val/loss", patience=1000
+            ),
+            lightning.pytorch.callbacks.DeviceStatsMonitor(cpu_stats=True),
+            lightning.pytorch.callbacks.ModelCheckpoint(
                 dirpath="../logs/RoofSense",
                 filename="best",
                 monitor="val/loss",
                 save_last=True,
             ),
-            EarlyStopping(monitor="val/loss", patience=1000),
+            lightning.pytorch.callbacks.OnExceptionCheckpoint(
+                dirpath="../logs/RoofSense"
+            ),
+            lightning.pytorch.callbacks.RichProgressBar(),
             LearningRateMonitor(),
         ],
         benchmark=True,
     )
 
     with warnings.catch_warnings():
-        warnings.filterwarnings(
-            "ignore",
-            category=UserWarning
-        )
+        warnings.filterwarnings("ignore", category=UserWarning)
         trainer.fit(model=task, datamodule=datamodule)
         trainer.test(model=task, datamodule=datamodule)
