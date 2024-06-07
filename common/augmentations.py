50f1c5cd4309c480866f2454a61c1ae0c9d87c60
Dimitris Mantas <dimitris.mantas@outlook.com>
2024-06-07 20:31:21 +0200

    Add HSV channels

--- common/augmentations.py
+++ common/augmentations.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
+import math
 from typing import Any, Dict, Literal, Optional, Tuple
 
+import kornia
 import kornia.augmentation as K
 import torch
 from kornia.geometry import hflip, vflip
@@ -132,6 +134,22 @@ class AppendGreenness(K.IntensityAugmentationBase2D):
 
         input = torch.cat((input, ag), dim=1)
         return input
+
+class AppendHSV(K.IntensityAugmentationBase2D):
+    "rgb data must be 0..1"
+    def __init__(self,*args,**kwargs):
+        super().__init__(p=1,*args,**kwargs)
+    def apply_transform(
+        self, input: Tensor, params: Dict[str, Tensor], flags: Dict[str, Any], transform: Optional[Tensor] = None
+    ) -> Tensor:
+        hsv=kornia.color.rgb_to_hsv(input[:, :3, ...])
+        # scale the H channel to from [0,2pi] to [0,1]
+        hsv[:,0,...]=hsv[:,0,...]/(2*math.pi)
+
+        input = torch.cat((input, hsv), dim=1)
+        return input
+
+
 class RandomSharpness(K.RandomSharpness):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
