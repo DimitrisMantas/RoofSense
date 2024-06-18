1e1aa10a3549f672edb13b1e3a151dcb1e2d9f43
Dimitris Mantas <dimitris.mantas@outlook.com>
2024-06-18 10:45:03 +0200

    Clean up code

--- utils/geom.py
+++ utils/geom.py
@@ -1,28 +1,13 @@
 import geopandas as gpd
 
 import config
-from utils.type import BoundingBoxLike
 
 
-# TODO: Harmonize this variable name.
-def read_surfaces(obj_id: str) -> gpd.GeoDataFrame:
+def read_surfaces(tile_id: str) -> gpd.GeoDataFrame:
     name = (
         f"{config.env('TEMP_DIR')}"
-        f"{obj_id}"
+        f"{tile_id}"
         f"{config.var('DEFAULT_SURFACES_FOOTPRINT_FILE_ID')}"
         f"{config.var('GEOPACKAGE')}"
     )
     return gpd.read_file(name)
-
-
-def buffer(geom: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
-    geom["geometry"] = geom["geometry"].buffer(float(config.var("BUFFER_DISTANCE")))
-    return geom
-
-
-def is_bbox_like(seq) -> bool:
-    return (
-        isinstance(seq, BoundingBoxLike.__origin__)
-        and len(seq) == 4
-        and all([(isinstance(i, int) or isinstance(i, float)) for i in seq])
-    )
