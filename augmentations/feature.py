0505489777f3dd294eab568d39fa590215b7d1ed
Dimitris Mantas <dimitris.mantas@outlook.com>
2024-08-08 07:54:26 +0300

    Finalize feature augmentations

--- augmentations/feature.py
+++ augmentations/feature.py
@@ -1,28 +1,44 @@
-from __future__ import annotations
+from typing import Any
 
-from typing import Any, Dict, Optional
-
-import kornia.augmentation as K
 import torch
-from overrides import override
+from kornia.augmentation import IntensityAugmentationBase2D
 from torch import Tensor
+from typing_extensions import override
 
 
-class MinMaxScaling(K.IntensityAugmentationBase2D):
-    def __init__(self, mins: Tensor, maxs: Tensor) -> None:
+class MinMaxScaling(IntensityAugmentationBase2D):
+    r"""Rescale batch-level features to the interval :math:`\left[0, 1\right]`."""
+
+    def __init__(self, mins: Tensor, maxs: Tensor, eps: float = 1e-12) -> None:
+        """Configure the scaler.
+
+        Args:
+            mins:
+                An 1D tensor containing the minimum value of each input channel.
+            maxs:
+                An 1D tensor containing the minimum value of each output channel.
+            eps:
+                A small floating-point value to avoid division-by-zero errors.
+
+        Warnings:
+            The input tensors are transferred to the batch device and cast to the
+            corresponding data type upon applying the transformation.
+        """
         super().__init__(p=1, same_on_batch=True)
-        self.flags = {"mins": mins.view(1, -1, 1, 1), "maxs": maxs.view(1, -1, 1, 1)}
-        self.delta = 1e-10
+        self.flags = {
+            "mins": mins.view(1, -1, 1, 1),
+            "maxs": maxs.view(1, -1, 1, 1),
+            "eps": eps,
+        }
 
-    # noinspection PyShadowingBuiltins
     @override
     def apply_transform(
         self,
         input: Tensor,
-        params: Dict[str, Tensor],
-        flags: Dict[str, Any],
-        transform: Optional[Tensor] = None,
+        params: dict[str, Tensor],
+        flags: dict[str, Any],
+        transform: Tensor | None = None,
     ) -> Tensor:
         mins = torch.as_tensor(flags["mins"], device=input.device, dtype=input.dtype)
         maxs = torch.as_tensor(flags["maxs"], device=input.device, dtype=input.dtype)
-        return (input - mins) / (maxs - mins + self.delta)
+        return (input - mins) / (maxs - mins + flags["eps"])
