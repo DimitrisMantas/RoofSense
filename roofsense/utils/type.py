8595cb1fa87e25f0bf977747070fad0c2026c275
Dimitris Mantas <dimitris.mantas@outlook.com>
2024-12-29 18:42:38 +0200

    Remove unused code

--- roofsense/utils/type.py
+++ roofsense/utils/type.py
@@ -4,10 +4,6 @@ from typing import Any, Literal, Required, TypedDict, Union
 
 import requests
 
-BAG3TileData = dict
-BAG3DTileIndexJSON = dict
-BAG3DTileIndexData = dict
-JSONLike = dict[str, Any]
 BoundingBoxLike = Sequence[float]
 Timeout = Union[float, tuple[float, float], tuple[float, None]]
 HookCallback = Callable[[requests.Response, ...], Any]
