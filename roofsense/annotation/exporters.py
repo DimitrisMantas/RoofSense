31942a9e5ffeb31d60ff2bfe368e3e7b24340cc4
Dimitris Mantas <dimitris.mantas@outlook.com>
2024-12-31 21:50:36 +0200

    Touch up exporters.py

--- roofsense/annotation/exporters.py
+++ roofsense/annotation/exporters.py
@@ -1,10 +1,27 @@
+import os
+from typing import Any
+
 import numpy as np
 import rasterio.plot
 
 
-def to_png(data: np.ndarray, path: str):
+def to_png(
+    data: np.ndarray[[Any, Any, Any], np.dtype[np.number]],
+    filepath: str | bytes | os.PathLike,
+) -> None:
+    """Export the RGB bands of the input raster to a PNG file.
+
+    Args:
+        data:
+            The input raster. Must contain at least three bands in the RGB configuration.
+        filepath:
+            The destination filepath.
+
+    Returns:
+        None
+    """
     with rasterio.open(
-        path,
+        filepath,
         mode="w",
         count=3,
         height=data.shape[1],
