41dbb779d764c1ac30c54a3250b41923e1a4dfb7
Dimitris Mantas <dimitris.mantas@outlook.com>
2025-02-13 09:44:03 +0200

    Actually add the inference examples

---
+++ implementation/inference/obia.py
@@ -0,0 +1,21 @@
+from roofsense.bag3d import BAG3DTileStore, LevelOfDetail
+from roofsense.inference.obia import MaskGeneralizer
+
+
+def main():
+    tile_id = "10-280-560"
+
+    generalizer = MaskGeneralizer(tile_store=BAG3DTileStore(dirpath=r"data/store"))
+    for name, lod in zip(
+        ["lod12", "lod13", "lod22"],
+        [LevelOfDetail.LoD12, LevelOfDetail.LoD13, LevelOfDetail.LoD22],
+    ):
+        generalizer.generalize(
+            src_filepath=rf"data/maps/pixel/{tile_id}.map.pixel.tif",
+            dst_filepath=rf"data/maps/obia/{tile_id}.map.{name}.tif",
+            lod=lod,
+        )
+
+
+if __name__ == "__main__":
+    main()
--- implementation/inference/tile.py
+++ implementation/inference/tile.py
@@ -1,19 +1,19 @@
 from terratorch.tasks.tiled_inference import TiledInferenceParameters
 
+from roofsense.bag3d import BAG3DTileStore
 from roofsense.inference.tile import TiledInferenceEngine
 
 
 def main():
-    tile_id = "9-272-552"
+    tile_id = "10-280-560"
+
     TiledInferenceEngine(
-        ckpt_path=r"C:\Documents\RoofSense\logs\optimization_random_search_round_3_tpe\version_20\ckpts\last.ckpt",
-        model_params={
-            "encoder_output_stride": 8,
-            "encoder_params": {"block_args": {"attn_layer": "eca"}},
-        },
+        ckpt_path=r"data/model/roofsense.paper.ckpt",
+        tile_store=BAG3DTileStore(dirpath=r"data/store"),
+        model_params={"encoder_params": {"block_args": {"attn_layer": "eca"}}},
     ).run(
         tile_id=tile_id,
-        dst_filename=rf"C:\Documents\RoofSense\dataset\infer\{tile_id}.map.tpe.stride16.terratorch.tif",
+        dst_filename=rf"data/maps/pixel/{tile_id}.map.pixel.tif",
         params=TiledInferenceParameters(
             h_crop=512, h_stride=256, w_crop=512, w_stride=256
         ),
