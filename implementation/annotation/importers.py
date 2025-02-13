80b929b0867a9d48dddc0c53fe48d79253b2c2c3
Dimitris Mantas <dimitris.mantas@outlook.com>
2025-02-13 09:44:35 +0200

    Add complete annotation examples

--- implementation/annotation/importers.py
+++ implementation/annotation/importers.py
@@ -7,12 +7,12 @@ def main():
     importer = RoboflowAnnotationImporter(
         # The name of the source directory generally depends on the structure defined by the annotation provider.
         # This example simulates Roboflow.
-        src_dirpath=r"./provider",
+        src_dirpath=r"data/provider",
         tile_store=BAG3DTileStore(),
     )
     importer.import_(
         # The dataset directory must already exist, and it should contain the original tiles in an appropriately named subdirectory according to 'importer.dst_image_dirname'.
-        dst_dirpath=r"./dataset",
+        dst_dirpath=r"data/dataset",
         # The example dataset is not suitable for stratified splitting due to its size.
         splitting_method=DatasetSplittingMethod.RANDOM,
     )
