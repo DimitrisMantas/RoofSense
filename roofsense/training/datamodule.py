5dd2ecbb1cf5fd1911aa923b657c05c1199961e4
Dimitris Mantas <dimitris.mantas@outlook.com>
2025-03-18 13:22:37 +0200

    clean up datamodule

--- roofsense/training/datamodule.py
+++ roofsense/training/datamodule.py
@@ -1,7 +1,9 @@
 import os
 import warnings
+from typing import cast
 
 import kornia.augmentation as K
+import lightning.pytorch.trainer.states
 import numpy as np
 import torch
 from kornia.constants import DataKey, Resample
@@ -32,40 +34,16 @@ class TrainingDataModule(NonGeoDataModule):
         super().__init__(
             TrainingDataset, batch_size=batch_size, num_workers=num_workers, **kwargs
         )
+        # Narrow down dataset class.
+        self.dataset_class = cast(type[TrainingDataset], self.dataset_class)
 
-        max_num_workers = max(1, os.cpu_count() // 2)
-        if num_workers is None:
-            self.num_workers = min(batch_size, max_num_workers)
-        elif batch_size < num_workers <= max_num_workers:
-            msg = (
-                f"Number of specified worker processes: {num_workers!r} greater than "
-                f"batch size: {batch_size!r}. Improved performance due to excess "
-                f"workers is not guaranteed."
-            )
-            warnings.warn(msg, UserWarning)
-        else:
-            msg = (
-                f"Number of specified worker processes: {num_workers!r} greater than "
-                f"half the total number of logical CPUs in the system: "
-                f"{max_num_workers!r}. Windows systems may suffer from erroneous "
-                f"behaviour and out-of-memory errors due to excessive memory paging. "
-                f"See See https://tinyurl.com/439cb683 and "
-                f"https://tinyurl.com/yc63wxey for more information."
-            )
-            warnings.warn(msg, ResourceWarning)
-
-        if self.num_workers == 0 and persistent_workers:
-            msg = (
-                "No worker processes specified to be persisted. Value of "
-                "'persistent_workers' will be set to 'False'."
-            )
-            warnings.warn(msg, UserWarning)
-            self.persistent_workers = False
-        else:
-            self.persistent_workers = persistent_workers
-
+        # Configure DataLoader arguments.
+        self._configure_num_workers()
+        self._configure_persistent_workers(persistent_workers)
         self.pin_memory = pin_memory
+        self.auto_drop_last = auto_drop_last
 
+        # Initialize augmentations.
         mins, maxs = torch.tensor_split(
             torch.from_numpy(
                 np.fromfile(
@@ -137,12 +115,48 @@ class TrainingDataModule(NonGeoDataModule):
 
     @override
     def setup(self, stage: str) -> None:
-        if stage in ["fit"]:
+        if stage in [lightning.pytorch.trainer.states.TrainerFn.FITTING]:
             self.train_dataset = self.dataset_class(split="training", **self.kwargs)
-        if stage in ["fit", "validate"]:
+        if stage in [
+            lightning.pytorch.trainer.states.TrainerFn.FITTING,
+            lightning.pytorch.trainer.states.TrainerFn.VALIDATING,
+        ]:
             self.val_dataset = self.dataset_class(split="validation", **self.kwargs)
-        if stage in ["test"]:
+        if stage in [lightning.pytorch.trainer.states.TrainerFn.TESTING]:
             self.test_dataset = self.dataset_class(split="test", **self.kwargs)
+
+    def _configure_num_workers(self):
+        max_num_workers = max(1, os.cpu_count() // 2)
+        if self.num_workers is None:
+            self.num_workers = min(self.batch_size, max_num_workers)
+        elif self.batch_size < self.num_workers <= max_num_workers:
+            msg = (
+                f"Number of specified worker processes: {self.num_workers!r} greater than "
+                f"batch size: {self.batch_size!r}. Improved performance due to excess "
+                f"workers is not guaranteed."
+            )
+            warnings.warn(msg, UserWarning)
+        else:
+            msg = (
+                f"Number of specified worker processes: {self.num_workers!r} greater than "
+                f"half the total number of logical CPUs in the system: "
+                f"{max_num_workers!r}. Windows systems may suffer from erroneous "
+                f"behaviour and out-of-memory errors due to excessive memory paging. "
+                f"See See https://tinyurl.com/439cb683 and "
+                f"https://tinyurl.com/yc63wxey for more information."
+            )
+            warnings.warn(msg, ResourceWarning)
+
+    def _configure_persistent_workers(self, per_workers):
+        if self.num_workers == 0 and per_workers:
+            msg = (
+                "No worker processes specified to be persisted. Value of "
+                "'persistent_workers' will be set to 'False'."
+            )
+            warnings.warn(msg, UserWarning)
+            self.persistent_workers = False
+        else:
+            self.persistent_workers = per_workers
 
     @override
     def _dataloader_factory(self, split: str) -> DataLoader[dict[str, Tensor]]:
